c35e1e680e1d8b479df445816f47251cedddec8e
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-02-15 05:28:34 -0300

    :sparkles: Configurando e Estilizando o Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
 
 	return (
 		<>
-			<div className="flex justify-center text-white bg-gradient-to-r from-gray-800 via-gray-700 to-[#1A1A1A]">
+			<div className="flex justify-center text-white bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900">
 				<div className="container flex flex-col items-center py-4">
 					<p className="text-xl font-bold">
 						Blog Pessoal Generation | Copyright:{' '}
@@ -15,13 +15,13 @@ function Footer() {
 						Acesse nossas redes sociais
 					</p>
 					<div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                        <a href="https://www.linkedin.com/in/carlos-h-dev/" target="_blank">
                             <LinkedinLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                        <a href="https://github.com/CrMessiProgrammer" target="_blank">
                             <GithubLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                        <a href="https://crmessiprogrammer.github.io/portfolio_tjs06/" target="_blank">
                             <EnvelopeSimple size={48} weight='bold' />
                         </a>
                     </div>

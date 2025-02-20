cf4aa8f0c89062fb7eece15afed3f23de2113780
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-02-20 14:19:46 -0300

    :sparkles: Estilização - Componente Formulário Postagens + Instalação da Biblioteca Reactjs Pop Up

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ListaTemas from './components/temas/listatemas/ListaTemas'
 import FormTema from './components/temas/formtema/FormTema'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 
 // Single Page Application (SPA - Aplicativo de Página Única)
 // Componente Funcional: Uma função JS ou TS que retornará uma aplicação renderizada no seu navegador.
@@ -34,7 +35,9 @@ function App() {
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
 							<Route path="/deletartema/:id" element={<DeletarTema />} />
-							<Route path="/postagens" element={<ListaPostagens />} />	
+							<Route path="/postagens" element={<ListaPostagens />} />
+							<Route path="/cadastrarpostagem" element={<FormPostagem />} />
+							<Route path="/editarpostagem/:id" element={<FormPostagem />} />	
 						</Routes>
 					</div>
 					<Footer />
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,13 @@
 import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
 
 function Home() {
-
     return (
         <>
-            <div className="flex justify-center bg-gradient-to-b from-gray-900 via-gray-800 to-[#3B5BB5]">
+            <div className="bg-gradient-to-b from-gray-900 via-gray-800 to-[#3B5BB5] flex justify-center">
                 <div className='container grid grid-cols-2 text-gray-100'>
-                    <div className="flex flex-col items-center justify-center gap-4 py-4">
-                        <h2 className='text-5xl max-sm:text-2xl font-bold'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
 							Bem Vindo(a)!
                         </h2>
                         <p className='text-xl'>
@@ -15,22 +15,22 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded border-white border-solid border-2 py-2 px-4 text-white'
-                                >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
 
-                    <div id="imagem" className="flex justify-center">
+                    <div className="flex justify-center ">
                         <img
                             src="./src/assets/home.svg"
-                            alt="Imagem da Página Home"
-                            className="w-2/3"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
                         />
                     </div>
                 </div>
             </div>
+
             <ListaPostagens />
         </>
     )
